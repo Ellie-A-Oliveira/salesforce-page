c50f56cfe6c5aaea0290c99721064c0866f3f782
--get-all <ellie.a.oliveira@gmail.com>
2024-03-29 10:12:27 -0300

    style(SF-107): fix styling of circle button

--- src/components/Circle-Button/Circle-Button.style.ts
+++ src/components/Circle-Button/Circle-Button.style.ts
@@ -1,14 +1,20 @@
 import { styled } from "styled-components";
+import { variables } from "../../styles/variables.style";
 
 export const CircleButtonStyled = styled.div<{
     color?: string
 }>`
     --size: 100px;
-    max-width: var(--size);
+    max-width: calc(var(--size) * 1.3);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
     p {
         word-break: keep-all;
         overflow-wrap: normal;
+        font-size: ${variables.textSize.small};
+        font-weight: bold;
     }
     .circle-button {
         color: ${(props) => props.color ?? "inherit"};
